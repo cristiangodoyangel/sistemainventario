423689cf8f7c5424755b35fb2f9ddca92cc4cddb
Star <creacciones@gmail.com>
2024-12-12 23:50:22 -0300

    - Initial commit

---
+++ src/test/java/cl/pmtintegra/demo/InventarioPmtApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.pmtintegra.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioPmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
