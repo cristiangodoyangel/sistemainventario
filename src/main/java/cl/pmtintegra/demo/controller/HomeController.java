9b07c022b5846881066cc28b8475cbe6fba74204
Star <creacciones@gmail.com>
2024-12-13 00:59:17 -0300

    - se crea base de datos: pmt_db
    y funciona

--- src/main/java/cl/pmtintegra/demo/controller/HomeController.java
+++ src/main/java/cl/pmtintegra/demo/controller/HomeController.java
@@ -1,3 +1,5 @@
+package cl.pmtintegra.demo.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
